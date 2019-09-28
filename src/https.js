b56c8ed54b39614f382d79eeadf9d7b09ce33469
Leahoop <504658355@qq.com>
2019-09-28 15:56:00 +0800

    review

--- src/https.js
+++ src/https.js
@@ -3,8 +3,8 @@ import qs from 'qs'
 
 axios.defaults.timeout = 5000;                        //响应时间
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';        //配置请求头
-axios.defaults.baseURL = 'http://localhost:8889';   //配置接口地址
-// axios.defaults.baseURL = 'http://47.101.42.58:8889';   //配置接口地址
+// axios.defaults.baseURL = 'http://localhost:8889';   //配置接口地址
+axios.defaults.baseURL = 'http://47.101.42.58:8889';   //配置接口地址
 
 //POST传参序列化(添加请求拦截器)
 axios.interceptors.request.use((config) => {
